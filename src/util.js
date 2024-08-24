14fb9a960f52c7427ab4fa559397556a094d9996
Adewale Stephen <adewalestephennifemi@gmail.com>
2024-08-24 22:51:25 +0100

    Create a javaScript file. This file serves as a jason file that pass information to my react app

---
+++ src/util.js
@@ -0,0 +1,27 @@
+export const user = {
+  name: "Jessica Randall",
+  location: "London, United Kingdom",
+  work: `"Front-end developer and avid reader"`,
+  links: [
+    {
+      account: "Github",
+      accountLink: "https://github.com/DemoStephen",
+    },
+    {
+      account: "Frontend Mentor",
+      accountLink: "https://www.frontendmentor.io/profile/DemoStephen",
+    },
+    {
+      account: "LinkedIn",
+      accountLink: "https://www.linkedin.com/in/stephen-adewale-96bb54229/",
+    },
+    {
+      account: "Twitter",
+      accountLink: "https://x.com/codewithstephen",
+    },
+    {
+      account: "Instagram",
+      accountLink: "https://www.instagram.com/s.t.e.p.h.e.n.n/",
+    },
+  ],
+};
